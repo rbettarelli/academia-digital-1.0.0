74c65e621945989e190ad453df7533a4efd4a93b
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2023-10-10 21:19:06 -0300

    entidades, relacionamentos

--- src/main/java/me/dio/academia/digital/entity/Aluno.java
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -4,10 +4,37 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_alunos")
+@JsonIgnoreProperties({"hybernateLazyInitializer", "handler"})
+
 public class Aluno {
 
+  @Id
+  @GeneratedValue(strategy= GenerationType.IDENTITY)
+ 
   private Long id;
 
+  @Column(unique = true)
   private String nome;
 
   private String cpf;
@@ -16,6 +43,8 @@ public class Aluno {
 
   private LocalDate dataDeNascimento;
 
+  @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
+  @JsonIgnore
   private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
 
 }
