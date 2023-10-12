3fd0e5ff6f8a0f3d583d7548dbb8ce1b34050687
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2023-10-11 21:01:39 -0300

    Avaliacao fisica controller

--- src/main/java/me/dio/academia/digital/repository/AvaliacaoFisicaRepository.java
+++ src/main/java/me/dio/academia/digital/repository/AvaliacaoFisicaRepository.java
@@ -1,4 +1,8 @@
 package me.dio.academia.digital.repository;
 
-public interface AvaliacaoFisicaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+
+public interface AvaliacaoFisicaRepository extends JpaRepository <AvaliacaoFisica, Long>{
 }
